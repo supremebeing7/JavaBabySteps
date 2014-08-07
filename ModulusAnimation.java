76a78c5b7d7f7f14bce1996d2e44a5bd2a4e10e8
Mark Lehman <markopolo@gmail.com>
2014-08-07 15:00:09 -0700

    add simple mod ascii train animation

---
+++ ModulusAnimation.java
@@ -0,0 +1,195 @@
+public class ModulusAnimation
+{
+	public static void main( String[] args ) throws Exception
+	{
+		for ( int i=0; i<40; i++ )
+		{
+			// Hacky way of clearing the screen
+			System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+			if ( i%20 == 0 )
+			{
+				System.out.print("  _______          \n");
+				System.out.print("  |  _  |    __    \n");
+				System.out.print("  | | | |____VV_   \n");
+				System.out.print("  | |_| |       7  \n");
+				System.out.print("  |_____|_______|  \n");
+				System.out.print("   00 00   00 00   \n");
+			}
+			else if ( i%20 == 1 )
+			{
+				System.out.print("   _______          \n");
+				System.out.print("   |  _  |    __    \n");
+				System.out.print("   | | | |____VV_   \n");
+				System.out.print("   | |_| |       7  \n");
+				System.out.print("   |_____|_______|  \n");
+				System.out.print("    00 00   00 00   \n");
+			}
+			else if ( i%20 == 2 )
+			{
+				System.out.print("    _______          \n");
+				System.out.print("    |  _  |    __    \n");
+				System.out.print("    | | | |____VV_   \n");
+				System.out.print("    | |_| |       7  \n");
+				System.out.print("    |_____|_______|  \n");
+				System.out.print("     00 00   00 00   \n");
+			}
+			else if ( i%20 == 3 )
+			{
+				System.out.print("     _______          \n");
+				System.out.print("     |  _  |    __    \n");
+				System.out.print("     | | | |____VV_   \n");
+				System.out.print("     | |_| |       7  \n");
+				System.out.print("     |_____|_______|  \n");
+				System.out.print("      00 00   00 00   \n");
+			}
+			else if ( i%20 == 4 )
+			{
+				System.out.print("      _______          \n");
+				System.out.print("      |  _  |    __    \n");
+				System.out.print("      | | | |____VV_   \n");
+				System.out.print("      | |_| |       7  \n");
+				System.out.print("      |_____|_______|  \n");
+				System.out.print("       00 00   00 00   \n");
+			}
+			else if ( i%20 == 5 )
+			{
+				System.out.print("       _______          \n");
+				System.out.print("       |  _  |    __    \n");
+				System.out.print("       | | | |____VV_   \n");
+				System.out.print("       | |_| |       7  \n");
+				System.out.print("       |_____|_______|  \n");
+				System.out.print("        00 00   00 00   \n");
+			}
+			else if ( i%20 == 6 )
+			{
+				System.out.print("        _______          \n");
+				System.out.print("        |  _  |    __    \n");
+				System.out.print("        | | | |____VV_   \n");
+				System.out.print("        | |_| |       7  \n");
+				System.out.print("        |_____|_______|  \n");
+				System.out.print("         00 00   00 00   \n");
+			}
+			else if ( i%20 == 7 )
+			{
+				System.out.print("         _______          \n");
+				System.out.print("         |  _  |    __    \n");
+				System.out.print("         | | | |____VV_   \n");
+				System.out.print("         | |_| |       7  \n");
+				System.out.print("         |_____|_______|  \n");
+				System.out.print("          00 00   00 00   \n");
+			}
+			else if ( i%20 == 8 )
+			{
+				System.out.print("          _______          \n");
+				System.out.print("          |  _  |    __    \n");
+				System.out.print("          | | | |____VV_   \n");
+				System.out.print("          | |_| |       7  \n");
+				System.out.print("          |_____|_______|  \n");
+				System.out.print("           00 00   00 00   \n");
+			}
+			else if ( i%20 == 9 )
+			{
+				System.out.print("           _______          \n");
+				System.out.print("           |  _  |    __    \n");
+				System.out.print("           | | | |____VV_   \n");
+				System.out.print("           | |_| |       7  \n");
+				System.out.print("           |_____|_______|  \n");
+				System.out.print("            00 00   00 00   \n");
+			}
+			else if ( i%20 == 10 )
+			{
+				System.out.print("            _______          \n");
+				System.out.print("            |  _  |    __    \n");
+				System.out.print("            | | | |____VV_   \n");
+				System.out.print("            | |_| |       7  \n");
+				System.out.print("            |_____|_______|  \n");
+				System.out.print("             00 00   00 00   \n");
+			}
+			else if ( i%20 == 11 )
+			{
+				System.out.print("             _______          \n");
+				System.out.print("             |  _  |    __    \n");
+				System.out.print("             | | | |____VV_   \n");
+				System.out.print("             | |_| |       7  \n");
+				System.out.print("             |_____|_______|  \n");
+				System.out.print("              00 00   00 00   \n");
+			}
+			else if ( i%20 == 12 )
+			{
+				System.out.print("              _______          \n");
+				System.out.print("              |  _  |    __    \n");
+				System.out.print("              | | | |____VV_   \n");
+				System.out.print("              | |_| |       7  \n");
+				System.out.print("              |_____|_______|  \n");
+				System.out.print("               00 00   00 00   \n");
+			}
+			else if ( i%20 == 13 )
+			{
+				System.out.print("               _______          \n");
+				System.out.print("               |  _  |    __    \n");
+				System.out.print("               | | | |____VV_   \n");
+				System.out.print("               | |_| |       7  \n");
+				System.out.print("               |_____|_______|  \n");
+				System.out.print("                00 00   00 00   \n");
+			}
+			else if ( i%20 == 14 )
+			{
+				System.out.print("                _______          \n");
+				System.out.print("                |  _  |    __    \n");
+				System.out.print("                | | | |____VV_   \n");
+				System.out.print("                | |_| |       7  \n");
+				System.out.print("                |_____|_______|  \n");
+				System.out.print("                 00 00   00 00   \n");
+			}
+			else if ( i%20 == 15 )
+			{
+				System.out.print("                 _______          \n");
+				System.out.print("                 |  _  |    __    \n");
+				System.out.print("                 | | | |____VV_   \n");
+				System.out.print("                 | |_| |       7  \n");
+				System.out.print("                 |_____|_______|  \n");
+				System.out.print("                  00 00   00 00   \n");
+			}
+			else if ( i%20 == 16 )
+			{
+				System.out.print("                  _______          \n");
+				System.out.print("                  |  _  |    __    \n");
+				System.out.print("                  | | | |____VV_   \n");
+				System.out.print("                  | |_| |       7  \n");
+				System.out.print("                  |_____|_______|  \n");
+				System.out.print("                   00 00   00 00   \n");
+			}
+			else if ( i%20 == 17 )
+			{
+				System.out.print("                   _______          \n");
+				System.out.print("                   |  _  |    __    \n");
+				System.out.print("                   | | | |____VV_   \n");
+				System.out.print("                   | |_| |       7  \n");
+				System.out.print("                   |_____|_______|  \n");
+				System.out.print("                    00 00   00 00   \n");
+			}
+			else if ( i%20 == 18 )
+			{
+				System.out.print("                    _______          \n");
+				System.out.print("                    |  _  |    __    \n");
+				System.out.print("                    | | | |____VV_   \n");
+				System.out.print("                    | |_| |       7  \n");
+				System.out.print("                    |_____|_______|  \n");
+				System.out.print("                     00 00   00 00   \n");
+			}
+			else if ( i%20 == 19 )
+			{
+				System.out.print("                     _______          \n");
+				System.out.print("                     |  _  |    __    \n");
+				System.out.print("                     | | | |____VV_   \n");
+				System.out.print("                     | |_| |       7  \n");
+				System.out.print("                     |_____|_______|  \n");
+				System.out.print("                      00 00   00 00   \n");
+			}
+
+
+			Thread.sleep(100);
+		}
+
+	}
+}
