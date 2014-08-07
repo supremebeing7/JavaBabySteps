93f26f41e85cbd5e17e6b25e540abc6174c48c7c
Mark Lehman <markopolo@gmail.com>
2014-08-07 11:22:02 -0700

    add SpaceBoxing program

---
+++ SpaceBoxing.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class SpaceBoxing
+{
+  public static void main( String[] args)
+  {
+    Scanner keyboard = new Scanner( System.in );
+
+    System.out.print( "Enter your current weight: " );
+    double weight = keyboard.nextDouble();
+
+    System.out.println( "\nI have information for the following planets:" );
+    System.out.println( "   1. Venus   2. Mars    3. Jupiter" );
+    System.out.println( "   4. Saturn  5. Uranus  6. Neptune" );
+
+    System.out.print( "\nWhich planet are you visiting? " );
+    int planet = keyboard.nextInt();
+
+    if (planet >= 1 && planet <= 6)
+    {
+      double space_weight = 1;
+      switch (planet) {
+        case 1: space_weight = weight * 0.78;
+        case 2: space_weight = weight * 0.39;
+        case 3: space_weight = weight * 2.65;
+        case 4: space_weight = weight * 1.17;
+        case 5: space_weight = weight * 1.05;
+        case 6: space_weight = weight * 1.23;
+      }
+      System.out.println( "\nYour weight would be " + space_weight + " on that planet." );
+    }
+    else
+    {
+      System.out.println( "\nInvalid planet selection" );
+      main(args);
+    }
+  }
+}
