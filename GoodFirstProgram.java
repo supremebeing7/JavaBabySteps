49642a0c006e1cfc2fc681e6f02c88b5d7eee05e
Mark Lehman <markopolo@gmail.com>
2014-08-06 22:39:05 -0700

    initial commit

---
+++ GoodFirstProgram.java
@@ -0,0 +1,8 @@
+public class GoodFirstProgram
+{
+  public static void main( String[] args )
+  {
+    System.out.println( "3 divided by 2 is " + ( 3.0 / 2.0 ));
+    System.out.println( "hey hey hey");
+  }
+}
---
+++ LearningVariables.java
@@ -0,0 +1,14 @@
+public class LearningVariables
+{
+  public static void main( String[] args )
+  {
+    int dogs = 3;
+    double kids = 4.0, headaches;
+    String exclamation = "Shissssa!";
+    headaches = dogs * kids;
+
+    System.out.println( "with " + dogs + " dogs and " + kids + " kids, I have ");
+    System.out.println( headaches + " headaches" );
+    System.out.println( exclamation );
+  }
+}
