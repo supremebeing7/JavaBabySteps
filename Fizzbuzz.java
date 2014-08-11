a3616876f0da54d833ce609b7a26e17fd1bb5f9a
Mark Lehman <markopolo@gmail.com>
2014-08-10 21:13:24 -0700

    add the most excellent game of FizzBuzz

---
+++ Fizzbuzz.java
@@ -0,0 +1,17 @@
+public class Fizzbuzz
+{
+  public static void main(String[] args)
+  {
+    for (int i = 1; i <= 100; i++)
+    {
+      if (i % 15 == 0)
+        System.out.println("Fizzbuzz");
+      else if (i % 5 == 0)
+        System.out.println("Buzz");
+      else if (i % 3 == 0)
+        System.out.println("Fizz");
+      else
+        System.out.println(i);
+    }
+  }
+}
