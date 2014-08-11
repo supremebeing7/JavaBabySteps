c3d314a16044579b23e3dc6841ca150e0e865404
Mark Lehman <markopolo@gmail.com>
2014-08-10 18:02:19 -0700

    refactor DiceDoubles to use DoWhile

--- DiceDoubles.java
+++ DiceDoubles.java
@@ -5,15 +5,10 @@ public class DiceDoubles
   public static void main(String[] args)
   {
     Random r = new Random();
-    int die1 = 1 + r.nextInt(6);
-    int die2 = 1 + r.nextInt(6);
+    int die1;
+    int die2;
 
-    System.out.println("HERE COMES THE DICE!\n");
-    System.out.println("Roll #1: " + die1);
-    System.out.println("Roll #2: " + die2);
-    System.out.println("Total: " + (die1 + die2));
-
-    while (die1 != die2)
+    do
     {
       die1 = 1 + r.nextInt(6);
       die2 = 1 + r.nextInt(6);
@@ -22,6 +17,6 @@ public class DiceDoubles
       System.out.println("Roll #1: " + die1);
       System.out.println("Roll #2: " + die2);
       System.out.println("Total: " + (die1 + die2));
-    }
+    } while (die1 != die2);
   }
 }
