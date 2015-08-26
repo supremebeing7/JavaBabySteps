7e8b691f93e7a2a96f592ca4125b76d55f061c7f
Mark Lehman <markopolo@gmail.com>
2015-08-26 15:55:31 -0700

    initial commit for leetspeak

---
+++ leetspeak/src/main/java/Leetspeak.java
@@ -0,0 +1,10 @@
+public class Leetspeak {
+  public static void main(String[] args) {}
+  // public Boolean isLeapYear(Integer year) {
+  //   if ( year % 400 == 0 ) {
+  //     return true;
+  //   } else {
+  //     return (year % 100 != 0) && (year % 4 == 0);
+  //   }
+  // }
+}
