e8943cef529722d8c99ecb4e47edde457e799a66
Mark Lehman <markopolo@gmail.com>
2015-08-26 17:31:27 -0700

    add leetspeak with passing specs

--- leetspeak/src/main/java/Leetspeak.java
+++ leetspeak/src/main/java/Leetspeak.java
@@ -1,10 +1,36 @@
 public class Leetspeak {
   public static void main(String[] args) {}
-  // public Boolean isLeapYear(Integer year) {
-  //   if ( year % 400 == 0 ) {
-  //     return true;
-  //   } else {
-  //     return (year % 100 != 0) && (year % 4 == 0);
-  //   }
-  // }
+
+  public String leetspeak(String phrase) {
+    String leetphrase = bulkReplaceLetters(phrase);
+    return fixIncorrectBulkChanges(leetphrase);
+  }
+
+  public String bulkReplaceLetters(String phrase) {
+    return phrase.replace("e", "3").
+                  replace("E", "3").
+                  replace("o", "0").
+                  replace("O", "0").
+                  replace("I", "1").
+                  replace("s", "z").
+                  replace("S", "Z");
+  }
+
+  public String fixIncorrectBulkChanges(String phrase) {
+    String[] wordsInPhrase = phrase.split(" ");
+    String recompiledPhrase = "";
+    for ( String word : wordsInPhrase ) {
+      recompiledPhrase = restoreLeadingSLetters(recompiledPhrase, word);
+    }
+    return recompiledPhrase.trim();
+  }
+
+  public String restoreLeadingSLetters(String phrase, String word) {
+    if ( word.substring(0,1).equals("z") ) {
+      phrase += ("s" + word.substring(1, word.length()));
+    } else {
+      phrase += word;
+    }
+    return phrase += " ";
+  }
 }
--- leetspeak/src/test/java/LeetspeakTest.java
+++ leetspeak/src/test/java/LeetspeakTest.java
@@ -2,27 +2,49 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class LeetspeakTest {
-  // @Test
-  // public void isLeapYear_forNumberDivisibleByFour_true() {
-  //   LeapYear testLeapYear = new LeapYear();
-  //   assertEquals(true, testLeapYear.isLeapYear(2012));
-  // }
-
-  // @Test
-  // public void isLeapYear_forNumberNotDivisibleByFour_false() {
-  //   LeapYear testLeapYear = new LeapYear();
-  //   assertEquals(false, testLeapYear.isLeapYear(2011));
-  // }
-
-  // @Test
-  // public void isLeapYear_forNumberDivisibleByOneHundred_false() {
-  //   LeapYear testLeapYear = new LeapYear();
-  //   assertEquals(false, testLeapYear.isLeapYear(1900));
-  // }
-
-  // @Test
-  // public void isLeapYear_forNumberDivisibleByFourHundred_true() {
-  //   LeapYear testLeapYear = new LeapYear();
-  //   assertEquals(true, testLeapYear.isLeapYear(2000));
-  // }
+  @Test
+  public void leetspeak_forWordWithNoLeetspeak_wordDoesNotChange() {
+    Leetspeak app = new Leetspeak();
+    assertEquals("fifty", app.leetspeak("fifty"));
+  }
+  @Test
+  public void leetspeak_forWordWithEs_replaceEs() {
+    Leetspeak app = new Leetspeak();
+    assertEquals("by3by3", app.leetspeak("byebye"));
+  }
+  @Test
+  public void leetspeak_forWordWithCapitalEs_replaceEs() {
+    Leetspeak app = new Leetspeak();
+    assertEquals("BY3BY3", app.leetspeak("BYEBYE"));
+  }
+  @Test
+  public void leetspeak_forWordWithOs_replaceOs() {
+    Leetspeak app = new Leetspeak();
+    assertEquals("b00m", app.leetspeak("boom"));
+  }
+  @Test
+  public void leetspeak_forWordWithCapitalOs_replaceOs() {
+    Leetspeak app = new Leetspeak();
+    assertEquals("B00M", app.leetspeak("BOOM"));
+  }
+  @Test
+  public void leetspeak_forWordWithIs_replaceIs() {
+    Leetspeak app = new Leetspeak();
+    assertEquals("1ndig0", app.leetspeak("Indigo"));
+  }
+  @Test
+  public void leetspeak_forWordWithSs_replaceSs() {
+    Leetspeak app = new Leetspeak();
+    assertEquals("hizz", app.leetspeak("hiss"));
+  }
+  @Test
+  public void leetspeak_forWordWithCapitalSs_replaceSs() {
+    Leetspeak app = new Leetspeak();
+    assertEquals("H1ZZ", app.leetspeak("HISS"));
+  }
+  @Test
+  public void leetspeak_makesCorrectReplacesmentsRegardlessOfCase_ignoresUppercase() {
+    Leetspeak app = new Leetspeak();
+    assertEquals( "1 scr3am y0u scr3am w3 all scr3am f0r razpb3rry ic3 cr3am!", app.leetspeak("I scream you scream we all scream for raspberry ice cream!"));
+  }
 }
