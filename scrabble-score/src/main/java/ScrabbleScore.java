4abfa9c34c55f4e2edc5590a88956cbbde96814d
Mark Lehman <markopolo@gmail.com>
2015-08-29 23:31:43 -0700

    add methods to compute score

--- scrabble-score/src/main/java/ScrabbleScore.java
+++ scrabble-score/src/main/java/ScrabbleScore.java
@@ -26,8 +26,66 @@ public class ScrabbleScore {
     }, new VelocityTemplateEngine());
   }
 
-  public static Integer calculateScore(String word) {}
+  public static Integer calculateScore(String word) {
+    String[] letters = word.split("");
+    Integer score = 0;
+    for ( String letter : letters ) {
+      score += letterPointValue(letter);
+    }
+    return score;
+  }
+
+  public static Integer letterPointValue(String letter) {
+    String lowerCaseLetter = letter.toLowerCase();
+    if ( onePointLetter(lowerCaseLetter) ) {
+      return 1;
+    } else if ( twoPointLetter(lowerCaseLetter) ) {
+      return 2;
+    } else if ( threePointLetter(lowerCaseLetter) ) {
+      return 3;
+    } else if ( fourPointLetter(lowerCaseLetter) ) {
+      return 4;
+    } else if ( fivePointLetter(lowerCaseLetter) ) {
+      return 5;
+    } else if ( eightPointLetter(lowerCaseLetter) ) {
+      return 8;
+    } else if ( tenPointLetter(lowerCaseLetter) ) {
+      return 10;
+    } else {
+      return 0;
+    }
+  }
+
+  public static Boolean onePointLetter(String letter) {
+    return "aeioulnrst".contains(letter);
+  }
+
+  public static Boolean twoPointLetter(String letter) {
+    return "dg".contains(letter);
+  }
+
+  public static Boolean threePointLetter(String letter) {
+    return "bcmp".contains(letter);
+  }
+
+  public static Boolean fourPointLetter(String letter) {
+    return "fhvwy".contains(letter);
+  }
+
+  public static Boolean fivePointLetter(String letter) {
+    return "k".equals(letter);
+  }
+
+  public static Boolean eightPointLetter(String letter) {
+    return "jx".contains(letter);
+  }
+
+  public static Boolean tenPointLetter(String letter) {
+    return "qz".contains(letter);
+  }
 }
+
+
 
 
 // Points:
--- scrabble-score/src/test/java/ScrabbleScoreTest.java
+++ scrabble-score/src/test/java/ScrabbleScoreTest.java
@@ -5,43 +5,43 @@ public class ScrabbleScoreTest {
   @Test
   public void calculateScore_forWordWithOnlyOnePointValueLetters_three() {
     ScrabbleScore testScrabbleScore = new ScrabbleScore();
-    assertEquals(3, testScrabbleScore.calculateScore("run"));
+    assertEquals((Integer)3, testScrabbleScore.calculateScore("run"));
   }
 
   @Test
   public void calculateScore_forWordWithOneOrTwoPointValueLetters_five() {
     ScrabbleScore testScrabbleScore = new ScrabbleScore();
-    assertEquals(5, testScrabbleScore.calculateScore("dog"));
+    assertEquals((Integer)5, testScrabbleScore.calculateScore("dog"));
   }
 
   @Test
   public void calculateScore_forWordWithOneTwoOrThreePointValueLetters_seven() {
     ScrabbleScore testScrabbleScore = new ScrabbleScore();
-    assertEquals(7, testScrabbleScore.calculateScore("pigs"));
+    assertEquals((Integer)7, testScrabbleScore.calculateScore("pigs"));
   }
 
   @Test
   public void calculateScore_forWordWithOneTwoThreeOrFourPointValueLetters_twelve() {
     ScrabbleScore testScrabbleScore = new ScrabbleScore();
-    assertEquals(12, testScrabbleScore.calculateScore("piggy"));
+    assertEquals((Integer)12, testScrabbleScore.calculateScore("piggy"));
   }
 
   @Test
   public void calculateScore_forWordWithOneTwoThreeFourOrFivePointValueLetters_fourteen() {
     ScrabbleScore testScrabbleScore = new ScrabbleScore();
-    assertEquals(14, testScrabbleScore.calculateScore("viking"));
+    assertEquals((Integer)14, testScrabbleScore.calculateScore("viking"));
   }
 
   @Test
   public void calculateScore_forWordWithOneTwoThreeFourFiveOrEightPointValueLetters_nineteen() {
     ScrabbleScore testScrabbleScore = new ScrabbleScore();
-    assertEquals(19, testScrabbleScore.calculateScore("jumpy"));
+    assertEquals((Integer)19, testScrabbleScore.calculateScore("jumpy"));
   }
 
   @Test
   public void calculateScore_forWordWithAnyPointValueLetters_twentyFive() {
     ScrabbleScore testScrabbleScore = new ScrabbleScore();
-    assertEquals(25, testScrabbleScore.calculateScore("quickly"));
+    assertEquals((Integer)25, testScrabbleScore.calculateScore("quickly"));
   }
 }
 
